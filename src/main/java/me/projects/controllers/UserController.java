a077a750bef0a64e43889fc2044c1f2b5a5e9f91
VilsonSouza <vilsonsouza758@gmail.com>
2023-10-19 08:59:08 -0300

    refactor: Adding annotatios to configure the swagger

--- src/main/java/me/projects/controllers/UserController.java
+++ src/main/java/me/projects/controllers/UserController.java
@@ -1,5 +1,6 @@
 package me.projects.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import me.projects.domain.model.User;
 import me.projects.services.UserService;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
+
 public class UserController {
 
     private final UserService userService;
@@ -20,13 +22,17 @@ public class UserController {
         this.userService = userService;
     }
 
+
     @GetMapping("/{id}")
+    @Operation(summary = "Find User by Id")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         var user = userService.findById(id);
         return ResponseEntity.ok(user);
     }
 
+
     @PostMapping
+    @Operation(summary = "Create a user")
     public ResponseEntity<User> create(@RequestBody User userToCreate) {
         var userCreated = userService.create(userToCreate);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
--- src/main/java/me/projects/domain/Application.java
+++ src/main/java/me/projects/domain/Application.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @OpenAPIDefinition(servers = {@Server(url = "/",description = "Default")})
 @SpringBootApplication
+@ComponentScan("me.projects")
 public class Application {
 
 	public static void main(String[] args) {
